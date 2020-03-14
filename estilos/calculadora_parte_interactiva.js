e458970a36be2092b55ee0f65a32be7c405cd63b
Edwin Carreño <eycarrenom@libertadores.edu.co>
2020-03-14 09:43:22 -0500

    subir codigo

---
+++ estilos/calculadora_parte_interactiva.js
@@ -0,0 +1,76 @@
+function init(){
+    alert();
+}
+
+//variables
+var
+
+
+//Eventos de java
+
+function Suma() {
+    var numero1 = +document.getElementById('text1').value;
+    var numero2 = +document.getElementById('text2').value;
+    window.alert(Number(numero1) + Number(numero2));
+}
+
+function Resta() {
+    var numero1 = +document.getElementById('text1').value;
+    var numero2 = +document.getElementById('text2').value;
+    window.alert(Number(numero1) - Number(numero2));
+}
+
+function Multiplicacion() {
+    var numero1 = +document.getElementById('text1').value;
+    var numero2 = +document.getElementById('text2').value;
+    window.alert(Number(numero1) * Number(numero2));
+}
+
+function Divide() {
+    var numero1 = +document.getElementById('text1').value;
+    var numero2 = +document.getElementById('text2').value;
+    window.alert(Number(numero1) / Number(numero2));
+}
+  
+
+function Numero_1() {
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '1';
+}
+function Numero_2(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '2';
+}
+function Numero_3(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '3';
+}
+function Numero_4(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '4';
+}
+function Numero_5(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '5';
+}
+function Numero_6(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '6';
+}
+function Numero_7(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '7';
+}
+function Numero_8(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '8';
+}
+function Numero_9(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '9';
+}
+function Numero_0(){
+    var numero1 = document.getElementById('text1');
+    numero1.value = numero1.value + '0';
+}
+    
